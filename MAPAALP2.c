28f882f44de176a001f7387b098366fa06d3f662
matheusdelnero <matheusdelnerovendas@gmail.com>
2022-07-08 16:32:02 -0300

    Update Final
    Alterações no tipo da variável 'cpf'.

--- MAPAALP2.c
+++ MAPAALP2.c
@@ -6,14 +6,14 @@ int opcao;
 int ncadastros;
 int i;
 int x = 1;
-char cpfprocura[11];
+char cpfprocura[19];
 int y = 1;
 int verdade;
 
 struct cadastro {
 	int codigo;
 	char nome[50];
-	int cpf;
+	char cpf[19];
 	char nomevacina[20];
 	char data[100];
 	int numerolote;
@@ -32,29 +32,29 @@ int main() {
 	while (opcao != 4){
 	switch(opcao){
 		case 1:
-		    printf("quantos cadastros deseja fazer?\n");
+		    printf("Quantos cadastros deseja fazer?\n");
 		    fflush(stdout);
 		    scanf("%d", &ncadastros);
 		    for (i = 1; i <= ncadastros; i = i + 1){
 		        lista[i].codigo = i;
 				fflush(stdout);
 				__fpurge(stdin);
-				printf("Digite o seu Nome %d :\n",i);
+				printf("Digite o seu Nome %d:\n",i);
 			    fflush(stdout);
 			    __fpurge(stdin);
 				gets(lista[i].nome);
-				printf("Digite o seu CPF:\n");
+				printf("Digite o seu CPF %d:\n", i);
 				fflush(stdout);
-				scanf("%d",&lista[i].cpf);
-				printf("Digite o nome da vacina:\n");
+				scanf("%s",&lista[i].cpf);
+				printf("Digite o Nome da Vacina %d:\n", i);
 				fflush(stdout);
 				__fpurge(stdin);
 				gets(lista[i].nomevacina);
-				printf("Digite a Data da Vacina:\n");
+				printf("Digite a Data da Vacina %d:\n", i);
 				fflush(stdout);
 				__fpurge(stdin);
 				gets(lista[i].data);
-				printf("Digite o numero de lote:\n");
+				printf("Digite o Numero de Lote %d:\n", i);
 				fflush(stdout);
 				__fpurge(stdin);
 				scanf("%d", &lista[i].numerolote);
@@ -68,7 +68,7 @@ int main() {
 			break;
 		case 2:
 				for (x = 1; x <= ncadastros; x = x + 1){
-					printf("------------------\nRegistro %d:\nCodigo: %d\nNome: %s\nCPF: %d\nNome da Vacina: %s\nData da vacina: %s\nNumero de Lote: %d\n------------------\n",x, lista[x].codigo, lista[x].nome, lista[x].cpf, lista[x].nomevacina, lista[x].data,lista[x].numerolote);
+					printf("------------------\nRegistro %d:\nCodigo: %d\nNome: %s\nCPF: %s\nNome da Vacina: %s\nData da vacina: %s\nNumero de Lote: %d\n------------------\n",x, lista[x].codigo, lista[x].nome, lista[x].cpf, lista[x].nomevacina, lista[x].data,lista[x].numerolote);
 				
 			}
 			printf("Oque deseja fazer agora?\n1 - Cadastrar Vacina\n2 - Listar Aplicacoes\n3 - Consultar por CPF\n4 - Sair\n");
@@ -80,16 +80,19 @@ int main() {
 		case 3:
 		    
 			printf("Digite o CPF o qual quer consultar cadastro:\n");
-			gets(cpfprocura);
+			fflush(stdout);
+			__fpurge(stdin);
+      gets(cpfprocura);
 			fflush(stdout);
 			__fpurge(stdin);
 			for (y = 1; y <= ncadastros; y = y + 1){
-			    verdade= strcmp(lista[y].cpf,cpfprocura == 0);
+			    verdade= strcmp(lista[y].cpf,cpfprocura);
 			    if (verdade==0){
-			        printf("CPF encontrado, segue cadastro:\nCodigo: %d\nNome: %s\nCPF: %d\nNome da Vacina: %s\nData da vacina: %s\nNumero de Lote: %d", lista[y].codigo, lista[y].nome, lista[y].cpf, lista[y].nomevacina, lista[y].data,lista[y].numerolote);
+			        printf("CPF encontrado, segue cadastro:\nCodigo: %d\nNome: %s\nCPF: %d\nNome da Vacina: %s\nData da vacina: %s\nNumero de Lote: %d\n\n", lista[y].codigo, lista[y].nome, lista[y].cpf, lista[y].nomevacina, lista[y].data,lista[y].numerolote);
 			    }
-			    else{
-			        printf("CPF não encontrado.\n");
+			    
+          else{
+			        printf("CPF nao encontrado.\n");
 			    }
 			
 			    
@@ -105,7 +108,7 @@ int main() {
 		
 	}
 }
-	printf("Obrigado por usar o programa de vacinas do Rio de Janeiro :)");
+	printf("Obrigado por usar o programa de vacinas do Rio de Janeiro! :)\n");
 	
 	
 	
